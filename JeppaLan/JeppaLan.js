592ada49a87cb5fd6a6fff2c30602637a192c212
XxKingAxelxX <axel.froborg@gmail.com>
2017-02-03 20:12:19 +0100

    Add files via upload

---
+++ JeppaLan/JeppaLan.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+
+function VisaBokning(visibility, visibility2) {
+	document.getElementById("Boka").style.display = visibility;
+	document.getElementById("Redo").style.display = visibility2;
+}
