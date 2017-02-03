2f753f49f6cc0625818ad9d0ec15016a881cc087
XxKingAxelxX <axel.froborg@gmail.com>
2017-02-03 22:00:15 +0100

    Add files via upload

--- JeppaLan/anmälningssidan.js
+++ JeppaLan/anmälningssidan.js
@@ -14,37 +14,22 @@ var platsen = 0;
 var tnr = "";
 var x;
 var klick = true;
+var y;
 
 function loadfunction(){
+	
 }
 
 function anmalan(rad, plats){
+	x = "p" + rad + plats; 
 	raden = rad;
 	platsen = plats;
-	
-	if(rad1namn[plats - 1] != ""){
-			document.getElementById("input").style.display = "none";
-
-	}
-	else if(rad2namn[plats - 1] != ""){
-			document.getElementById("input").style.display = "none";
-
-	}
-	else if(rad3namn[plats - 1] != ""){
-			document.getElementById("input").style.display = "none";
-
-	}
-
-	else{
 	document.getElementById("input").style.display = "block";
 	document.getElementById("bordnummer").innerHTML = rad + "." + plats;
-
 	namn = "";
 	tnr = "";
 	document.getElementById("namninput").value = "";
 	document.getElementById("nrinput").value = "";
-		
-}
 }
 
 function avbryt(){
@@ -59,33 +44,28 @@ function inputnr(val){
 }
 
 function anmal(){
-
-	if(namn != "" && tnr != ""){
-	switch(raden)
+	
+	y = x + "a"
+	
+	document.getElementById(x).style.backgroundColor = "red";
+	document.getElementById(y).innerHTML = namn;
+	 switch(raden)
 		{
 			case '1':
 				rad1namn[platsen-1] = namn;
 				rad1tnr[platsen-1] = tnr;
-				x = "p1" + platsen;
-				document.getElementById(x).style.backgroundColor = "red";
-				
 				break;
 			case '2':
 				rad2namn[platsen-1] = namn;
 				rad2tnr[platsen-1] = tnr;
-				x = "p2" + platsen;
-				document.getElementById(x).style.backgroundColor = "red";
 				break;
 			case '3':
 				rad3namn[platsen-1] = namn;
 				rad3tnr[platsen-1] = tnr;
-				x = "p3" + platsen;
-				document.getElementById(x).style.backgroundColor = "red";
 				break;
-		}
 		document.getElementById("input").style.display = "none";
 	}
 	else{
-		window.alert("Fyll i namn och telefonnummer");
+		window.alert("");
 	}
 }
